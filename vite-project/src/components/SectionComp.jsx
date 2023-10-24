791e546256bf9a000c43858ab1b76be05a0e1633
TLRKiliann <philogenie@protonmail.com>
2023-10-24 13:54:25 +0200

    colors improved

--- vite-project/src/components/SectionComp.jsx
+++ vite-project/src/components/SectionComp.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
 export default function SectionComp(props) {
+  
+  const handleClick = () => {
+    alert("Clicked !");
+  }
+
   return (
     <>
       <section className={props.className}>
         <article>
           <h2>{props.title}</h2>
-          <p>{props.paragraph}</p>
+          <div className='paragraph-menu'>
+            <p>{props.paragraph}</p>
+            <button type="button" onClick={handleClick}>Read</button>
+          </div>
         </article>
       </section>
     </>
