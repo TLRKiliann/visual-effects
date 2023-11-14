2ad65a2e56787c4f1eb95fba23eed4205e2b6dac
TLRKiliann <philogenie@protonmail.com>
2023-11-14 14:02:53 +0100

    styles scss done

--- vite-project/src/components/DropDownComp.tsx
+++ vite-project/src/components/DropDownComp.tsx
@@ -5,7 +5,7 @@ export default function DropDownComp() {
   const menuDropdown = () => {
     const rubriques = document.querySelectorAll('aside > ul > li > a');
     Array.prototype.forEach.call( rubriques, (rubrique) => {
-      rubrique.onclick = ( e ) => {
+      rubrique.onclick = ( e: React.MouseEvent<HTMLButtonElement> ) => {
         e.preventDefault();
         let li = rubrique.parentNode; // ul
         let openHeight = li.querySelector('ul').scrollHeight; // adjust automaticaly
@@ -32,36 +32,36 @@ export default function DropDownComp() {
 
   return (
     <>
-        <div className='dropdown--menu'>
-          <aside>
-            <ul>
-                <li className='dropdown--tilte'>
-                  <a onClick={menuDropdown}>Story of Nier</a>
-                    <ul>
-                        <li><a href="#">Draken Guard 1 - 2 - 3</a></li>
-                        <li><a href="#">Nier Gestalt</a></li>
-                        <li><a href="#">Nier Automata</a></li>
-                    </ul>
-                </li>
-                <li className='dropdown--tilte'>
-                  <a onClick={menuDropdown}>Heros</a>
-                    <ul>
-                        <li><a href="#">9S</a></li>
-                        <li><a href="#">2B</a></li>
-                        <li><a href="#">A2</a></li>
-                    </ul>
-                </li>
-                <li className='dropdown--tilte'>
-                  <a onClick={menuDropdown}>Bonus</a>
-                    <ul>
-                        <li><a href="#">Animation</a></li>
-                        <li><a href="#">Game Play</a></li>
-                        <li><a href="#">Goodies</a></li>
-                    </ul>
-                </li>
-            </ul>
-          </aside>
-        </div>
+      <div className='dropdown--menu'>
+        <aside>
+          <ul>
+              <li className='dropdown--tilte'>
+                <a onClick={menuDropdown}>Story of Nier</a>
+                  <ul>
+                      <li><a href="#">Draken Guard 1 - 2 - 3</a></li>
+                      <li><a href="#">Nier Gestalt</a></li>
+                      <li><a href="#">Nier Automata</a></li>
+                  </ul>
+              </li>
+              <li className='dropdown--tilte'>
+                <a onClick={menuDropdown}>Heros</a>
+                  <ul>
+                      <li><a href="#">9S</a></li>
+                      <li><a href="#">2B</a></li>
+                      <li><a href="#">A2</a></li>
+                  </ul>
+              </li>
+              <li className='dropdown--tilte'>
+                <a onClick={menuDropdown}>Bonus</a>
+                  <ul>
+                      <li><a href="#">Animation</a></li>
+                      <li><a href="#">Game Play</a></li>
+                      <li><a href="#">Goodies</a></li>
+                  </ul>
+              </li>
+          </ul>
+        </aside>
+      </div>
     </>
   )
 }
